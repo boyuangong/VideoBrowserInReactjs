70be6b1542ba7570e89b59e72981d3f3d563e74b
boyuangong <boyuangong@tamu.edu>
2018-07-25 00:56:39 -0500

    add searchBar and Youtube API_KEY

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
 
-const App  = () => {
-  return <div> Hi </div>
-};
+const API_KEY = 'AIzaSyCAp6SONDJGV7IaymbUJyzSA8bLHifHdAk';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (vidoes) => {
+            this.setState({ vidoes });
+            // this.setState({videos: videos}); The same thing
+
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 
 ReactDOM.render(<App />, document.querySelector(".container"));
