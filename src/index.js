b456753c8e9e0bacf04558dc86b1832786b7a6bb
boyuangong <boyuangong@tamu.edu>
2018-07-25 00:56:39 -0500

    Merge search branch
    add searchBar and Youtube API_KEY
    
    add videoList to fetch display the video
    
    add videoListItem with video snippet title and image
    
    display fetched video and can play for the first one
    
    add select video(call back function) and css style
    
    Make searchBar real work
    
    Add lodash to limit the refresh time gap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
+import _ from 'lodash';
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetails';
 
-const App  = () => {
-  return <div> Hi </div>
-};
+const API_KEY = 'AIzaSyCAp6SONDJGV7IaymbUJyzSA8bLHifHdAk';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+    
+        this.videoSearch('Drakes');
+    }
+    
+    videoSearch(term) {
+	    YTSearch({key: API_KEY, term: term}, (videos) => {
+		    this.setState({
+			    videos: videos,
+			    selectedVideo: videos[0]
+		    });
+		    // this.setState({videos: videos}); The same thing
+	    });
+    }
+    
+    render() {
+        const videoSearch = _.debounce((iterm) => {this.videoSearch(term)}, 300);
+      
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+	                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                  videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
 
 ReactDOM.render(<App />, document.querySelector(".container"));
